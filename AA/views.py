8ae556db7cf5e1b43cbd7183e5ddadd630a8e4a4
Michael Isah <michaelisahadav@gmail.com>
2017-08-23 12:22:48 +0100

    Final Changes

--- AA/views.py
+++ AA/views.py
@@ -3,19 +3,3 @@ from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
 def index(request):
     return HttpResponseRedirect("/poll/")
-
-
-# def handler404(request):
-#     return render(request, "404.html")
-#
-#
-# def handler400(request):
-#     return render(request, "400.html")
-#
-#
-# def handler403(request):
-#     return render(request, "403.html")
-#
-#
-# def handler500(request):
-#     return render(request, "500.html")
